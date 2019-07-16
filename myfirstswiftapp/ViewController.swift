360492954031a379b81d91875f037f082bc82360
Sophia Lee <leezhang0709@gmail.com>
2019-07-16 14:54:23 -0700

    Setup Launchscreen

---
+++ myfirstswiftapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myfirstswiftapp
+//
+//  Created by Sophia Lee on 2019/7/15.
+//  Copyright © 2019 Sophia Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
