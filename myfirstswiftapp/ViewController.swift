d3fb0f6b80a1b6c4828e02e57dcb3d43a66efc25
Sophia Lee <leezhang0709@gmail.com>
2019-07-17 12:39:02 -0700

    random switch code completed

--- myfirstswiftapp/ViewController.swift
+++ myfirstswiftapp/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    var image1 :Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+        
+    @IBAction func imageClick(_ sender: Any) {
+        if (image1 == false){
+            imageView1.image = UIImage(named: "Snorlax")
+            image1 = true
+        }
+        else {
+        imageView1.image = UIImage(named: "Eevee")
+            image1 = false
+        }
+        
+    }
+    
 }
 
